fbf79512313957b7138112a833383a44e9a54345
RaihanMalay21 <raihanmalay14@gmail.com>
2024-12-19 07:24:47 -0800

    update comment layanan audit

--- src/Components/LayananAudit/page.js
+++ src/Components/LayananAudit/page.js
@@ -19,13 +19,23 @@ export const PageLayananAudit = () => {
             margin: 'auto',
             paddingBottom: '7.5vh'
         },
+        parentText: {
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center',
+            borderRadius: '5px',
+            backgroundColor: '#f0f4f7',
+            padding: '20px',
+            boxShadow: '1.5px 3px 8px rgba(169, 169, 169, 0.8)',
+            zIndex: '100'
+        },
         Text : {
             fontSize: '18px',
             marginTop: '20px',
             margin: '0 2rem',
             marginTop: '20px',
             color: '#333',
-            textAlign: 'center'
+            textAlign: 'center',
         }
     }
     return(
@@ -74,14 +84,18 @@ export const PageLayananAudit = () => {
                 </div>
                 <div className="container-fill-sectionTwooLK">
                     <div className="header-sectionTwooLK" style={{ textAlign: 'center'}}>Keunggulan Layanan Kami</div>
-                    <div style={style.Text}>
-                        <p>Untuk memberikan layanan audit yang terbaik, kami telah menjalin kemitraan dengan beberapa kantor akuntan publik. Dengan dukungan dari para ahli ini, kami dapat memastikan bahwa proses audit berjalan lancar dan hasilnya dapat diandalkan. Kemitraan ini juga memungkinkan kami untuk memberikan layanan yang lebih komprehensif dan sesuai dengan kebutuhan spesifik perusahaan Anda.</p>
+                    <div style={style.parentText}> 
+                        <div style={style.Text}>
+                            <p>Untuk memberikan layanan audit yang terbaik, kami telah menjalin kemitraan dengan beberapa kantor akuntan publik. Dengan dukungan dari para ahli ini, kami dapat memastikan bahwa proses audit berjalan lancar dan hasilnya dapat diandalkan. Kemitraan ini juga memungkinkan kami untuk memberikan layanan yang lebih komprehensif dan sesuai dengan kebutuhan spesifik perusahaan Anda.</p>
+                        </div>
                     </div>
                 </div>
                 <div className="container-fill-sectionTwooLK" style={{marginBottom: '10vh'}}>
                     <div className="header-sectionTwooLK" style={{ textAlign: 'center'}}>Hubungi Kami</div>
-                    <div style={style.Text}>
-                        <p>Jika Anda tertarik untuk mengetahui lebih lanjut tentang layanan general audit kami atau ingin menjadwalkan konsultasi, jangan ragu untuk menghubungi kami. Tim kami siap membantu Anda mencapai transparansi dan akuntabilitas yang lebih baik dalam bisnis Anda.</p>
+                    <div style={style.parentText}>
+                        <div style={style.Text}>
+                            <p>Jika Anda tertarik untuk mengetahui lebih lanjut tentang layanan general audit kami atau ingin menjadwalkan konsultasi, jangan ragu untuk menghubungi kami. Tim kami siap membantu Anda mencapai transparansi dan akuntabilitas yang lebih baik dalam bisnis Anda.</p>
+                        </div>
                     </div>
                 </div>
                 <Footer/>
--- src/Components/Product/layanan-pajak/sectionFive-LP.js
+++ src/Components/Product/layanan-pajak/sectionFive-LP.js
@@ -1,14 +1,57 @@
-import "../../../App.css"
+import { useState } from "react";
+import "../../../App.css";
+import img1 from "../../../images/Logo DT.png";
+import img2 from "../../../images/Logo MC.png";
+import img3 from "../../../images/Logo MS.png";
 
 function SectionFiveLP() {
+    const [isIndex, setIndex] = useState(0);
+    const reviews = [
+        {
+          image: img1, 
+          comment: "“Dengan Finfast, saya tidak perlu lagi pusing mengurus laporan keuangan setiap bulan!”",
+          name: "Devtex"
+        },
+        {
+          image: img2, 
+          comment: "“Sangat membantu UMKM seperti saya dalam pengelolaan pajak dan keuangan.”",
+          name: "Collection"
+        },
+        {
+          image: img3, 
+          comment: "“Proses keuangan jadi lebih mudah, cepat, dan aman dengan Finfast.”",
+          name: "Mandi Shop"
+        }
+      ];
+
+      const handelCommentLeft = () => {
+        setIndex((prevIndex) => (prevIndex === 0 ? reviews.length - 1 : prevIndex - 1));
+      };
+
+      const handleCommentRight = () => {
+        setIndex((prevIndex) => (prevIndex === reviews.length - 1 ? 0 : prevIndex + 1 ));
+      };
+
     return(
         <div className="container-sectionFiveLP"> 
             <div className="container-fill-sectionFiveLP">
                 <h2>Kata pemilik umkm tentang layanan pajak Finfast</h2>
                 <div className="container-box-sectionFiveLP" style={{ backgroundColor: 'white'}}>
-                    <img src={require("../../../images/umkm-review.jpg")}/>
-                    <p style={{ fontSize: "1.2rem"}}>“Enaknya dengan Finfast, urusan keuangan dan pajak bisa autopilot”</p>
-                    <p>Raihan Malay</p>
+                    <div style={{ alignContent: 'center'}}>
+                        <svg className="svg-icon"  onClick={() => handelCommentLeft()} style={{cursor: 'pointer'}} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                        <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+                        </svg>
+                    </div>
+                    <div className="content-comment-lp">
+                        <img src={reviews[isIndex].image}/>
+                        <p style={{ fontSize: "1.2rem"}}>{reviews[isIndex].comment}</p>
+                        <p>{reviews[isIndex].name}</p>
+                    </div>
+                    <div style={{ alignContent: 'center'}}>
+                        <svg className="svg-icon"  style={{cursor: 'pointer'}} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                        <path onClick={() => handleCommentRight()} d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                        </svg>
+                    </div>
                 </div>
             </div>
         </div>
